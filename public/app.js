1bf417378167d319b695c72430ed04bf24899612
Your GitHub Username <davilama@unlv.nevada.edu>
2024-04-08 16:29:56 -0700

    finished class work

--- public/app.js
+++ public/app.js
@@ -2,12 +2,19 @@
 
 
 
-// Create map
+// Create map:                                                       
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+                                                     
 
 
 
 // Add openstreetmap tiles
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 
@@ -16,10 +23,17 @@
 
 // Create and add a geolocation marker
 
-
+//const marker = L.marker([48.87007, 2.346453])
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 
 // Draw the 2nd arrondissement
+                                         
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
 
 
 
@@ -29,4 +43,46 @@
 
 
 
+
 // Create red pin marker
+//const redPinMarker = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
+
+
+
+// Creating a red Circle
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+
+
+//Creating Pop-ups
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+
+// Click and tells you the what location you are 
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+
+
+
+//More advanced pop up for where you click
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
