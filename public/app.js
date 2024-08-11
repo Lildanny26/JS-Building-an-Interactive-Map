9f313cbff60d1bfcae00cadcf4a66371300c45e2
Your GitHub Username <davilama@unlv.nevada.edu>
2024-08-11 10:01:20 -0700

    finished HW

--- public/app.js
+++ public/app.js
@@ -1,88 +1,63 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
+// Create map
+// let map = L.map('map').setView([51.505, -0.09], 13);    /*from the quick start guide. */
 
-
-// Create map:                                                       
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-                                                     
-
-
+var myMap = L.map('map').setView([48.87007, 2.346453], 15);
 
 // Add openstreetmap tiles
+// L.tileLayer('https://.tile.openstreetmap.org///.png', {    /*from the quick start guide. */
+//     maxZoom: 15,
+//     attribution: '<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
+}).addTo(myMap);
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
 
 // Create and add a geolocation marker
 
-//const marker = L.marker([48.87007, 2.346453])
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// let marker = L.marker([51.1, -0.09]).addTo(map);
+
+let marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()  /*this looks like what is causing error */
 
 
 // Draw the 2nd arrondissement
-                                         
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
 
+let polygon = L.polygon([                                         
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]
+]).addTo(myMap);
 
-
-
-// Metro station markers
-
-
-
-
-
-// Create red pin marker
-//const redPinMarker = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
-
-
-
-// Creating a red Circle
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
-
-
-//Creating Pop-ups
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-
-
-// Click and tells you the what location you are 
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
-
-map.on('click', onMapClick);
-
-
-
-//More advanced pop up for where you click
 var popup = L.popup();
 
+/*popup on click */
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+        .openOn(myMap);
 }
 
-map.on('click', onMapClick);
+myMap.on('click', onMapClick);
+// Metro station markers
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
+
+
+// Create red pin marker
